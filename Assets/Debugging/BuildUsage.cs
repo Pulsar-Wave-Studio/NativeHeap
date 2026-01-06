c8b02f15ddfe786e11abbd09fcd9a36b4552abce
PWS-david <pulsarwavedavid@gmail.com>
2026-01-06 15:53:48 +0100

    feat: BurstCompiled job usage to make sure the heap is burst compatible

--- Assets/Debugging/BuildUsage.cs
+++ Assets/Debugging/BuildUsage.cs
@@ -1,22 +1,55 @@
 using Amarcolina.NativeHeap;
+using Unity.Burst;
 using Unity.Collections;
+using Unity.Jobs;
 using UnityEngine;
 
 
 // Make sure this script is used in a scene to avoid the NativeHeap being stripped from a build.
 public class BuildUsage : MonoBehaviour
 {
-    private NativeHeap<int, Min> _heap;
+    private UsageJob _job;
+    private JobHandle _handle;
+    private bool _isJobScheduled;
 
 
-    void Awake()
+    void Start()
     {
-        _heap = new NativeHeap<int, Min>(Allocator.Persistent);
+        _job = new UsageJob()
+        {
+            heap = new NativeHeap<int, Min>(Allocator.TempJob)
+        };
+        _handle = _job.Schedule();
+        JobHandle.ScheduleBatchedJobs();
+
+        _isJobScheduled = true;
     }
 
-    void OnDestroy()
+    private void Update()
     {
-        _heap.Dispose();
-        _heap = default;
+        if (!_isJobScheduled)
+        {
+            return;
+        }
+
+        _handle.Complete();
+        _handle = default;
+
+        Debug.Log($"Heap contents: {_job.heap.Pop()}");
+        _job.heap.Dispose();
+        _job = default;
+
+        _isJobScheduled = false;
+    }
+
+    [BurstCompile(CompileSynchronously = true)]
+    public struct UsageJob : IJob
+    {
+        public NativeHeap<int, Min> heap;
+
+        public void Execute()
+        {
+            heap.Insert(42);
+        }
     }
 }
