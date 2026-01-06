df96a8909f6f352af29e0bbdd12aa758ce477443
PWS-david <pulsarwavedavid@gmail.com>
2026-01-06 15:03:15 +0100

    feat: set up scene and build settings for testing building the NativeHeap for a player

---
+++ Assets/Debugging/BuildUsage.cs
@@ -0,0 +1,21 @@
+using Unity.Collections;
+using UnityEngine;
+
+
+// Make sure this script is used in a scene to avoid the NativeHeap being stripped from a build.
+public class BuildUsage : MonoBehaviour
+{
+    private NativeHeap<int, Min> _heap;
+
+
+    void Awake()
+    {
+        _heap = new NativeHeap<int, Min>(Allocator.Persistent);
+    }
+
+    void OnDestroy()
+    {
+        _heap.Dispose();
+        _heap = default;
+    }
+}
